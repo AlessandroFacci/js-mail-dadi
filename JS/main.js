cdc8e9dd37d3c667e2fa46e91c6269b9a826e22f
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-24 14:38:05 +0200

    js: add mail

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+// CREO UNA LISTA DI MAIL
+
+// * arrey
+const mail = [
+  "mail1@gmail.com",
+  "mail2@gmail.com",
+  "mail2@gmail.com",
+  "mail3@gmail.com",
+  "mail4@gmail.com",
+  "mail5@gmail.com",
+];
+console.log(mail);
